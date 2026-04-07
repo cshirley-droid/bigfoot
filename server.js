639a3a24fb8c92257511f40ab7861932a514913e
cshirley-droid <cshirley@wwps.org>
2026-04-06 20:53:04 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+import ServerPkg from 'boardgame.io/dist/cjs/server.js';
+const { Server, Origins } = ServerPkg;
+
+// Ensure your Game.js only contains game logic, and no React or CSS imports!
+import { BigfootGame } from './src/Game.js'; 
+
+const server = Server({
+  games: [BigfootGame],
+  origins: [Origins.LOCALHOST], 
+});
+
+const PORT = process.env.PORT || 8000;
+
+server.run(PORT, () => {
+  console.log(`🌲 Bigfoot Server is running on port ${PORT}...`);
+});
